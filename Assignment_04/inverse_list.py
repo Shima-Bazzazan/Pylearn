f997013f5abc48407f9708b264a37e13529fe92a
Shima-Bazzazan <149187804+Shima-Bazzazan@users.noreply.github.com>
2023-11-05 21:24:59 +0330

    A_04

---
+++ Assignment_04/inverse_list.py
@@ -0,0 +1,20 @@
+
+
+n= int(input("Please enter the length of list: "))
+
+list = [] 
+inverse_list = []  
+
+print("Please enter the members of the list: ")
+
+while n>0:
+    
+    list.append(int(input()))
+    n-=1
+
+print("List= ",list)
+
+for i in range(len(list)-1, -1, -1):
+    inverse_list.append(list[i])
+
+print("Inverse List =",inverse_list)
