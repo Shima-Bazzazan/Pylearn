02567b28e9534955315cfd8c80f7ec8ac36fa9f0
Shima-Bazzazan <149187804+Shima-Bazzazan@users.noreply.github.com>
2023-11-30 14:40:46 +0330

    Delete Assignment_04 directory

--- Assignment_04/Qrcode.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-name = input("Please enter your Name: ")
-last_name=input("Please enter your Lastname: ")
-phone_number = input("Please enter your phone number: ")
-
-img = qrcode.make(name+" "+last_name +"\n"+phone_number)
-img.save("Shimdal_qrcode.png")
