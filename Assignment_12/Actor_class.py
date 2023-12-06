c5c3a693c38e878ddb0c750ab4cf736b0ffa381e
Shima-Bazzazan <149187804+Shima-Bazzazan@users.noreply.github.com>
2023-12-06 23:44:35 +0330

    A_12

---
+++ Assignment_12/Actor_class.py
@@ -0,0 +1,9 @@
+
+class Actor:
+    def __init__(self,name,lastname):
+        self.name = name
+        self.lastname= lastname
+        
+
+    def show(self):
+        print(self.name , self.lastname)
