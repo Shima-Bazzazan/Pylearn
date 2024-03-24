2e7f395cdd897c9372047add64e1520c05183690
Shima-Bazzazan <149187804+Shima-Bazzazan@users.noreply.github.com>
2024-03-24 16:32:28 +0330

    Update tv_noise.py

--- Assignment_27/tv_noise/tv_noise.py
+++ Assignment_27/tv_noise/tv_noise.py
@@ -3,7 +3,7 @@ import numpy as np
 import imageio
 
 
-image = cv2.imread("picture\old_tv.jpg")
+image = cv2.imread("old_tv.jpg")
 image = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
 
 rows, cols=image.shape
