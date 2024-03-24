e1b6512cfd08aa3720a31279fcc0d98fda8fa62c
Shima-Bazzazan <149187804+Shima-Bazzazan@users.noreply.github.com>
2024-03-24 16:22:11 +0330

    Update tv_noise.py

--- Assignment_27/tv_noise/tv_noise.py
+++ Assignment_27/tv_noise/tv_noise.py
@@ -3,30 +3,22 @@ import numpy as np
 import imageio
 
 
-init_image = cv2.imread("tv_noise\picture\old_tv.jpg")
-img = cv2.cvtColor(init_image , cv2.COLOR_BGR2GRAY)
+image = cv2.imread("picture\old_tv.jpg")
+image = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
 
-rows, cols=img.shape
+rows, cols=image.shape
 
-writer = cv2.VideoWriter("noise.mp4" , cv2.VideoWriter_fourcc(*'mp4v') , 30 , (rows , cols))
-
-print(img.shape)
+frames = []
 while True:
-    noise1 = np.random.random(( 72, 99)) * 255 
-    noise1 = np.array(noise1 , dtype=np.uint8)
-    img[163:235 , 875:974] = noise1
+    noise = np.random.random((164 , 216)) * 255 
+    noise = np.array(noise , dtype=np.uint8)
+    image[179:343 , 317:533] = noise
 
-    noise2 = np.random.random((100 , 135)) * 255 
-    noise2 = np.array(noise2 , dtype=np.uint8)
-    img[130:230 , 400:535] = noise2
+    new_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    frames.append(new_image)
 
-    noise2 = np.random.random((134 , 162)) * 255 
-    noise2 = np.array(noise2 , dtype=np.uint8)
-    img[391:525 , 325:487] = noise2
-    writer.write(img)
-
-    cv2.imshow("tv_noise" , img)
-    if cv2.waitKey(25) & 0xFF == ord("b"):
+    cv2.imshow("tv_noise" , new_image)
+    if cv2.waitKey(25) & 0xFF == ord("q"):
         break
 
-writer.release()
+imageio.mimsave ("tv_noise.gif", frames)
